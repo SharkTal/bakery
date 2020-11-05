24dfef5ef52936841ae7431a10483ddec8292549
SharkTal <liutao.gao@gmail.com>
2020-11-05 22:30:26 +0200

    maven plugins bug fix

--- src/main/java/com/example/bakery/BakeryApplication.java
+++ src/main/java/com/example/bakery/BakeryApplication.java
@@ -5,10 +5,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class BakeryApplication {
+public class BakeryApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder
+														 application) {
+		return application.sources(BakeryApplication.class);
+	}
+
 	@Autowired
 	private BreadRepository breadRepository;
 
